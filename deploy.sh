7af2533e178d8dc9bac43e9506b2db73a87a6743
matkeller <mat.keller@gmail.com>
2019-03-28 13:28:30 -0600

    First commit of Gateway and LAC files and deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+echo INFO: Deploying Gateway and MySQL
+kubectl apply -f container-gateway-config.yml -f container-gateway-mysql.yml -f container-gateway.yml 
+sleep 5
+
+echo INFO: Deploying Live API Creator
+kubectl apply -f lac-nps.yml
+sleep 5
+
+echo INFO: Exposing Gateway ports
+kubectl expose deployment gw-dc --type=NodePort
+
+echo 
+kubectl get service
+
+echo
+kubectl get pods
+
+echo INFO: Please wait for Gateway to start...
